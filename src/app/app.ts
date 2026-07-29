8d3c32d51af59874dffb7215fa0f534fcfaaf578
Johnny Souto <johnny.souto@totvs.com.br>
2026-07-29 07:09:42 -0300

    feat: add location component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,14 @@ import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { Reasons } from './components/reasons/reasons';
 import { Schedule } from './components/schedule/schedule';
+import { Location } from './components/location/location';
 import { Faq } from './components/faq/faq';
 import { Cta } from './components/cta/cta';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, Reasons, Schedule, Faq, Cta, Footer],
+  imports: [Header, Hero, Reasons, Schedule, Location, Faq, Cta, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
